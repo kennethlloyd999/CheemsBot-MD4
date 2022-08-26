262a325632cf6ce731b1729379590621a3d9abe0
kennethlai666 <108507064+kennethlai666@users.noreply.github.com>
2022-08-26 21:21:43 +0700

    Add files via upload

--- XeonCheems4.js
+++ XeonCheems4.js
@@ -780,10 +780,21 @@ kice = m.sender
 await XeonBotInc.groupParticipantsUpdate(m.chat, [kice], 'remove')
 } else {
 }
+
+if (budy.includes("⿻")) {
+if (!isBotAdmins) return reply(`\`\`\`「 Virus Detected 」\`\`\``)
+bvl = `\`\`\`「 Virus Detected 」\`\`\``
+if (isAdmins) return reply(bvl)
+if (m.key.fromMe) return reply(bvl)
+if (isCreator) return reply(bvl)
+kice = m.sender
+await XeonBotInc.groupParticipantsUpdate(m.chat, [kice], 'remove')
+} else {
+}
 //antivirtex by xeon
   if (budy.length > 1500) {
   	if (!isBotAdmins) return reply(`\`\`\`「 Virus Detected 」\`\`\``)
-  if (!isAdmins) return
+  if (!isAdmins) return reply(❤)
   let kice = m.sender
   await XeonBotInc.groupParticipantsUpdate(m.chat, [kice], 'remove')
   XeonBotInc.sendMessage(from, {text:`\`\`\`「 Virus Detected 」\`\`\`\n\n Haha You Will Be Kicked! 🐒`, contextInfo:{mentionedJid:[kice]}}, {quoted:m})
