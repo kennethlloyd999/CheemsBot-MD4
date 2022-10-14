1de2aade329f9af7e5a18337c31a6ba2c4978342
kennethlai666 <108507064+kennethlai666@users.noreply.github.com>
2022-10-14 16:24:41 +0700

    Add files via upload

--- XeonCheems4.js
+++ XeonCheems4.js
@@ -4959,8 +4959,8 @@ const bakk = args.join(" ")
 const baksd = args.join(" ")
 if (!bakk) return replay(`en = inggris\nin = indonesia\n\nExample:\n${prefix + command} en *text*`)
 if (!baksd) return replay(`en = inggris\nin = indonesia\n\nExample:\n${prefix + command} en *text*`)
-var ka = bakk.split(' ')[0]
-var ko = baksd.split(' ')[1];
+const ka = bakk.split(' ')[0];
+const ko = baksd.split(' ')[1];
 tes = await fetchJson (`https://megayaa.herokuapp.com/api/translate?to=${ka}&kata=${ko}`)
 Infoo = tes.info
 Detek = tes.translate
